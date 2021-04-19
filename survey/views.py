9ff5e9078f0054ead3119b5c6f60c2edf741ed7a
rakibul bashar <rakibul.gononet@gmail.com>
2021-04-19 17:38:54 +0600

    added pagination in quiz list view

--- survey/views.py
+++ survey/views.py
@@ -1,25 +1,31 @@
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
-from .models import Quiz, Option
-from .serializers import QuizListPageSerializer, QuizDetailPageSerializer, OptionSerializer, VoteSerializer, QuizResultPageSerializer
-
+from .models import Option, Quiz
+from .serializers import (OptionSerializer, QuizDetailPageSerializer,
+                          QuizListPageSerializer, QuizResultPageSerializer,
+                          VoteSerializer)
 
 
 @api_view(['GET', 'POST'])
 def quizzes_view(request):
     if request.method == 'GET':
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
         quizs = Quiz.objects.all()
-        serializer = QuizListPageSerializer(quizs, many=True)
+        result_page = paginator.paginate_queryset(quizs, request)
+        serializer = QuizListPageSerializer(result_page, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = QuizListPageSerializer(data=request.data)
         if serializer.is_valid():
             quiz = serializer.save()
-            return Response(QuizListPageSerializer(quiz).data, status=status.HTTP_201_CREATED)
+            return Response(QuizListPageSerializer(quiz).data,
+                            status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -30,7 +36,9 @@ def quiz_detail_view(request, alias):
         serializer = QuizDetailPageSerializer(quiz)
         return Response(serializer.data)
     elif request.method == 'PATCH':
-        serializer = QuizDetailPageSerializer(quiz, data=request.data, partial=True)
+        serializer = QuizDetailPageSerializer(quiz,
+                                              data=request.data,
+                                              partial=True)
         if serializer.is_valid():
             quiz = serializer.save()
             return Response(QuizDetailPageSerializer(quiz).data)
@@ -46,7 +54,8 @@ def options_view(request, alias):
     serializer = OptionSerializer(data=request.data)
     if serializer.is_valid():
         option = serializer.save(quiz=quiz)
-        return Response(OptionSerializer(option).data, status=status.HTTP_201_CREATED)
+        return Response(OptionSerializer(option).data,
+                        status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -55,14 +64,17 @@ def vote_view(request, alias):
     quiz = get_object_or_404(Quiz, alias=alias)
     serializer = VoteSerializer(data=request.data)
     if serializer.is_valid():
-        option = get_object_or_404(Option, pk=serializer.validated_data['option_id'])
+        option = get_object_or_404(Option,
+                                   pk=serializer.validated_data['option_id'])
         option.vote_count += 1
         option.save()
         return Response("Voted")
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def quiz_result_view(request, alias):
     quiz = get_object_or_404(Quiz, alias=alias)
     serializer = QuizResultPageSerializer(quiz)
     return Response(serializer.data)
+
--- task_project/settings.py
+++ task_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'survey',
     'rest_framework',
 ]
@@ -72,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'task_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -83,25 +79,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
@@ -116,7 +114,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+# REST FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
 
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
